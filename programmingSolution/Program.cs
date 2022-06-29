e404c5c6b8e0c528f900cd3ef36ca45cd44c3aa2
ge64kem <ge64kem@mytum.de>
2022-06-29 11:33:34 +0200

    added method for determining length of result array

--- programmingSolution/Program.cs
+++ programmingSolution/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// input: 
+string[] input = { "1234", "1679", "089", "Maksim", "Max" };
+
+int resultLength = 0;
+
+// Getting length of result array
+resultLength = lengthOfResult(input); 
+ int lengthOfResult(String[] array){
+int result = 0; 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        result++;
+    }
+    i++;
+}
+return result; 
+}
+
