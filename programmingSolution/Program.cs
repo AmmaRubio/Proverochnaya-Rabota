76d32a79c2052e8144c39177e42f2d635b9f670b
ge64kem <ge64kem@mytum.de>
2022-06-29 11:48:40 +0200

    programming solution is done and tested

--- programmingSolution/Program.cs
+++ programmingSolution/Program.cs
@@ -4,17 +4,60 @@ string[] input = { "1234", "1679", "089", "Maksim", "Max" };
 int resultLength = 0;
 
 // Getting length of result array
-resultLength = lengthOfResult(input); 
- int lengthOfResult(String[] array){
-int result = 0; 
-for (int i = 0; i < array.Length; i++)
+resultLength = lengthOfResult(input);
+int lengthOfResult(string[] array)
 {
-    if (array[i].Length <= 3)
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
     {
-        result++;
+        if (array[i].Length <= 3)
+        {
+            result++;
+        }
+        i++;
     }
-    i++;
-}
-return result; 
+    return result;
 }
 
+// Creating result array
+string[] result = new string[resultLength];
+
+// Populating result array 
+result = keepingSmallerThanThreeChar(input);
+string[] keepingSmallerThanThreeChar(string[] array)
+{
+    string[] result = new string[lengthOfResult(array)];
+    int j = 0; // j - indexing of result array
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[j] = array[i];
+            j++;
+        }
+        i++;
+    }
+    return result;
+}
+
+// Printing array
+void printArrayInLine(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"'{array[i]}'");
+        }
+        else
+        {
+            Console.Write($"'{array[i]}', ");
+        }
+    }
+    Console.Write("]");
+}
+
+printArrayInLine(input); 
+Console.Write(" -> ");
+printArrayInLine(result); 
